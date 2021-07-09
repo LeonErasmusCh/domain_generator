6b8166ffad969dcd4f996b89a520fbf437e0b51f
LeonErasmusCh <leon@santiagobiltong.cl>
2021-07-09 01:28:09 +0000

    Domain generator V1.0

--- src/app.js
+++ src/app.js
@@ -1,11 +1,12 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+  var pronoun = ['the','our'];
+  var adj = ['great', 'big' ];
+  var noun = ['jogger','racoon'];
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+  for(let i = 0; i < pronoun.length; i++){
+      for(let j = 0; j < adj.length; j++){
+        for(let x = 0; x < noun.length; x++){
+        console.log(pronoun[i],adj[j],noun[x], ".cl");
+        }
+    }
+  };
